645bd5d4195155404b1788116fb0a954d045088d
florian <dinter.florian@googlemail.com>
2020-02-07 16:16:14 +0100

    small example

---
+++ FormsExample/ContenPageExample.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FormsExample
+{
+    public partial class ContenPageExample : ContentPage
+    {
+        public ContenPageExample()
+        {
+            InitializeComponent();
+        }
+    }
+}
